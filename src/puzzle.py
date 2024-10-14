0db34c8b62c00a595a3ac5e73cda56e27ddc6ba7
siok <eladiohv2@gmail.com>
2024-10-14 12:07:38 -0500

    Added test_move

--- src/puzzle.py
+++ src/puzzle.py
@@ -40,6 +40,12 @@ class puzzle:
 
 
         return board, position
+    
+    def set_board(self, new_board):
+        self.board = new_board
+
+    def get_board(self):
+        return self.board
 
     def move(self, direction):
         '''
